8bb847270fa5dbc49ad2c5f130fbccf2a4084a83
abhijit kalta <abhijitkalta@gmail.com>
2015-10-28 18:31:52 +0530

    Marked as and delete functionality added

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,6 @@
 class TodoItemsController < ApplicationController
 before_action :set_todo_list
+before_action :set_todo_item, except: [:create]
 
 def create
   @todo_item = @todo_list.todo_items.create(todo_items_params)
@@ -7,13 +8,17 @@ def create
 end
 
 def destroy
-  @todo_item = @todo_list.todo_items.find(params[:id])
   if @todo_item.destroy
     flash[:success] = "Todo Item successfully deleted"
   else
     flash[:error] = "Todo Item couldn't be deleted"
   end
   redirect_to @todo_list
+end
+
+def complete
+  @todo_item.update_attribute(:completed_at , Time.now)
+  redirect_to @todo_list, notice: "Todo item completed"
 end
 
 
@@ -27,4 +32,9 @@ end
 def set_todo_list
   @todo_list = TodoList.find(params[:todo_list_id])
 end
+
+def set_todo_item
+  @todo_item = @todo_list.todo_items.find(params[:id])
+end
+
 end
